eac35a3c7b08dc8c8b97ae18d1926e5075e636f4
julian <jjhennessy1231@gmail.com>
2020-10-28 01:11:55 -0500

    Forgot to save before the last commit, this is the updated version

--- src/Cube.java
+++ src/Cube.java
@@ -1,4 +1,29 @@
 
 public class Cube {
-
+	
+	//TODO: Figure out a way to store the position of a Rubik's cube
+	
+	public void R(boolean prime) {
+		
+	}
+	
+	public void U(boolean prime) {
+		
+	}
+	
+	public void F(boolean prime) {
+		
+	}
+	
+	public void L(boolean prime) {
+		
+	}
+	
+	public void D(boolean prime) {
+		
+	}
+	
+	public void B(boolean prime) {
+		
+	}
 }
